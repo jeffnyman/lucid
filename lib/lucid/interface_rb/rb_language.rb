d27aa70a17ee42438b13127e9dfb39538ccbdd31
Jeff Nyman <jeffnyman@gmail.com>
2013-04-01 07:35:09 -0500

    Ability to parse TDL-level methods.

--- lib/lucid/interface_rb/rb_language.rb
+++ lib/lucid/interface_rb/rb_language.rb
@@ -1,6 +1,15 @@
+require "lucid/interface_rb/rb_lucid"
+
 module Lucid
   module InterfaceRb
+    # This module is the Ruby implementation of the TDL API.
     class RbLanguage
+
+      # Get the expressions of various I18n translations of TDL keywords.
+      # In this case the TDL is based on Gherkin.
+      Gherkin::I18n.code_keywords.each do |adverb|
+        RbLucid.alias_adverb(adverb)
+      end
 
       def initialize(facade)
         @runtime_facade = facade
