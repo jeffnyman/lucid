863a16892246afef3b6621819f0a80aedd8e4fc6
Jeff Nyman <jeffnyman@gmail.com>
2013-04-27 18:22:28 -0500

    Reworked unicode to be Lucid specific.

--- lib/lucid/formatter/unicode.rb
+++ lib/lucid/formatter/unicode.rb
@@ -4,8 +4,8 @@ require 'lucid/platform'
 require 'lucid/formatter/ansicolor'
 
 if Lucid::WINDOWS
-  if ENV['CUCUMBER_OUTPUT_ENCODING']
-    Lucid::CODEPAGE = ENV['CUCUMBER_OUTPUT_ENCODING']
+  if ENV['LUCID_OUTPUT_ENCODING']
+    Lucid::CODEPAGE = ENV['LUCID_OUTPUT_ENCODING']
   elsif `cmd /c chcp` =~ /(\d+)/
     if [65000, 65001].include? $1.to_i
       Lucid::CODEPAGE = 'UTF-8'
@@ -15,7 +15,7 @@ if Lucid::WINDOWS
     end
   else
     Lucid::CODEPAGE = "cp1252"
-    STDERR.puts("WARNING: Couldn't detect your output codepage. Assuming it is 1252. You may have to chcp 1252 or SET CUCUMBER_OUTPUT_ENCODING=cp1252.")
+    STDERR.puts("WARNING: Unable to detect your output codepage; assuming it is 1252. You may have to chcp 1252 or SET LUCID_OUTPUT_ENCODING=cp1252.")
   end
 
   module Lucid
@@ -24,23 +24,23 @@ if Lucid::WINDOWS
       def self.extended(o)
         o.instance_eval do
 
-          def cucumber_preprocess_output(*a)
+          def lucid_preprocess_output(*a)
             begin
               a.map{|arg| arg.to_s.encode(Encoding.default_external)}
             rescue Encoding::UndefinedConversionError => e
-              STDERR.cucumber_puts("WARNING: #{e.message}")
+              STDERR.lucid_puts("WARNING: #{e.message}")
               a
             end
           end
 
-          alias cucumber_print print
+          alias lucid_print print
           def print(*a)
-            cucumber_print(*cucumber_preprocess_output(*a))
+            lucid_print(*lucid_preprocess_output(*a))
           end
 
-          alias cucumber_puts puts
+          alias lucid_puts puts
           def puts(*a)
-            cucumber_puts(*cucumber_preprocess_output(*a))
+            lucid_puts(*lucid_preprocess_output(*a))
           end
         end
       end
