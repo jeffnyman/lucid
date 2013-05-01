5edd6c5e880eeb0b17babf82dbe58efdaa08e7be
Jeff Nyman <jeffnyman@gmail.com>
2013-05-01 06:03:59 -0500

    Files can be added to the project structure.

--- lib/lucid/generator.rb
+++ lib/lucid/generator.rb
@@ -5,10 +5,16 @@ module Lucid
   class Generator < Thor
     desc "project NAME", "Create a new project."
 
+    method_option :browser, aliases: "-b", type: :boolean, desc: "Use for browser-based testing."
+    method_option :driver, aliases: "-d", type: :string, required: false, desc: "Framework driver to use. (Default value is 'symbiont'.)"
+
     def project(name)
       puts "Name of project: #{name}"
 
-      Lucid::Generators::Project.start([name])
+      driver = options[:driver].nil? ? 'symbiont' : options[:driver]
+      browser = options[:browser] == true ? 'true' : 'false'
+
+      Lucid::Generators::Project.start([name, browser, driver])
     end
   end
 end
--- lib/lucid/generators/project.rb
+++ lib/lucid/generators/project.rb
@@ -5,12 +5,19 @@ module Lucid
     class Project < Thor::Group
       include Thor::Actions
 
-      argument :name, type: :string, desc: "Name of the project."
+      argument :name,    type: :string,  desc: "Name of the project."
+      argument :browser, type: :string,  desc: "Use for browser-based testing."
+      argument :driver,  type: :string,  desc: "Framework driver to use."
 
       desc "Generates a project structure."
 
+      def self.source_root
+        File.dirname(__FILE__) + "/project"
+      end
+
       def spit_back_values
         puts "Create project '#{name}'"
+        puts "Use for browser-based apps." if use_browser
       end
 
       def create_project_directory
@@ -28,6 +35,15 @@ module Lucid
         empty_directory("#{name}/pages")
       end
 
+      def copy_errors
+        copy_file "errors.rb", "#{name}/common/support/errors.rb"
+      end
+
+    private
+
+      def use_browser
+        browser == 'true'
+      end
     end
   end
 end
