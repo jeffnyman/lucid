8ad9fd0610d5eaff211e2c8c8b8209d046f5d27d
Jeff Nyman <jeffnyman@gmail.com>
2013-10-25 18:51:21 -0500

    Facade tests.

---
+++ spec/lucid/facade_spec.rb
@@ -0,0 +1,31 @@
+require_relative '../spec_helper'
+
+module Lucid
+  describe Runtime::Facade do
+    
+    let(:interface) {double('interface')}
+    subject { Runtime::Orchestrator.new(interface,{}) }
+    let(:facade) { Runtime::Facade.new(subject, interface) }
+    
+    it 'should produce AST::Table by #table' do
+      facade.table( %{
+      | study   | phase |
+      | test-01 | I     |
+      | test-02 | II    |
+      } ).should be_kind_of(AST::Table)
+    end
+
+    it 'should produce AST::DocString by #doc_string with default content-type' do
+      str = facade.doc_string('TEST')
+      str.should be_kind_of(AST::DocString)
+      str.content_type.should eq('')
+    end
+
+    it 'should produce AST::DocString by #doc_string with ruby content-type' do
+      str = facade.doc_string('TEST','ruby')
+      str.should be_kind_of(AST::DocString)
+      str.content_type.should eq('ruby')
+    end
+    
+  end
+end
