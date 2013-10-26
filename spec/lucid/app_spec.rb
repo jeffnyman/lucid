cc4409a6237a1a0afb52bd8594b6426ee2e930d8
Jeff Nyman <jeffnyman@gmail.com>
2013-10-26 08:21:38 -0500

    App start tests.

--- spec/lucid/app_spec.rb
+++ spec/lucid/app_spec.rb
@@ -3,7 +3,7 @@ require_relative '../spec_helper'
 module Lucid
   module CLI
     describe App do
-      
+
       let(:args)   { [] }
       let(:stdin)  { StringIO.new }
       let(:stdout) { StringIO.new }
@@ -33,6 +33,56 @@ module Lucid
             runtime.stub(:results).and_return(results)
             kernel.should_receive(:exit).with(1)
             do_start
+          end
+        end
+
+        context 'execution is interrupted' do
+          after do
+            Lucid.wants_to_quit = false
+          end
+
+          it 'should register as a failure' do
+            results = double('results', :failure? => false)
+            runtime = Runtime.any_instance
+            runtime.stub(:run)
+            runtime.stub(:results).and_return(results)
+
+            Lucid.wants_to_quit = true
+            kernel.should_receive(:exit).with(1)
+            subject.start
+          end
+        end
+      end
+      
+      describe 'verbose execution' do
+        before(:each) do
+          b = Lucid::Parser::TDLBuilder.new('specs/test.spec')
+          b.feature(Gherkin::Formatter::Model::Feature.new([], [], 'Feature', 'Testing', '', 99, ''))
+          b.language = double
+          @empty_feature = b.result
+        end
+
+        it 'should show the spec files that were parsed' do
+          cli = App.new(%w{--verbose test.spec}, stdin, stdout, stderr, kernel)
+          cli.stub(:require)
+
+          Lucid::SpecFile.stub(:new).and_return(double('spec file', :parse => @empty_feature))
+          kernel.should_receive(:exit).with(0)
+
+          cli.start
+
+          stdout.string.should include('test.spec')
+        end
+      end
+      
+      describe 'handling exceptions' do
+        [ProfilesNotDefinedError, YmlLoadError, ProfileNotFound].each do |exception_class|
+          it "rescues #{exception_class}, prints the message to the error stream" do
+            Configuration.stub(:new).and_return(configuration = double('configuration'))
+            configuration.stub(:parse).and_raise(exception_class.new('error message'))
+            
+            subject.start
+            stderr.string.should == "error message\n"
           end
         end
       end
