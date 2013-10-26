3cca21a75b874a05a574aa8eda1a77afae747fff
Jeff Nyman <jeffnyman@gmail.com>
2013-10-26 09:28:35 -0500

    Configuration tests.

--- spec/lucid/configuration_spec.rb
+++ spec/lucid/configuration_spec.rb
@@ -24,6 +24,8 @@ module Lucid
   
   module CLI
     describe Configuration do
+
+      attr_reader :out, :error
       
       module ExposeOptions
         attr_reader :options
@@ -39,12 +41,13 @@ module Lucid
         Dir.stub(:[]).and_return(files)
       end
       
-      def with_this_configuration_file(info)
+      def with_this_configuration(info)
+        Dir.stub(:glob).with('{,.config/,config/}lucid{.yml,.yaml}').and_return(['lucid.yml'])
         File.stub(:exist?).and_return(true)
         profile_file = info.is_a?(Hash) ? info.to_yaml : info
         IO.stub(:read).with('lucid.yml').and_return(profile_file)
       end
-      
+
       it 'should require driver.rb files first' do
         with_these_files('/common/support/browser.rb', '/common/support/driver.rb')
         config.parse(%w{--require /common})
@@ -142,6 +145,12 @@ module Lucid
         config.parse(%w{--verbose})
         config.options[:verbose].should be_true
       end
+
+      it 'uses the default profile when no profile is defined' do
+        with_this_configuration({'default' => '--require test_file'})
+        config.parse(%w{--format progress})
+        config.options[:require].should include('test_file')
+      end
       
       describe 'generating output' do
       
@@ -166,6 +175,130 @@ module Lucid
         Lucid::Term::ANSIColor.should_receive(:coloring=).with(false)
         config = Configuration.new(StringIO.new)
         config.parse(['--no-color'])
+      end
+
+      it 'should accept multiple --format options and put the STDOUT one first so progress is seen' do
+        config.parse(%w{--format standard --out output.txt --format progress})
+        config.formats.should == [['progress', out], ['standard', 'output.txt']]
+      end
+
+      it 'should not accept multiple --format options when both use implicit STDOUT' do
+        lambda do
+          config.parse(%w{--format pretty --format progress})
+        end.should raise_error('All but one formatter must use --out, only one can print to each stream (or STDOUT)')
+      end
+
+      it 'should accept same --format options with implicit STDOUT, and keep only one' do
+        config.parse(%w{--format standard --format standard})
+        config.formats.should == [['standard', out]]
+      end
+
+      it 'should not accept multiple --out streams pointing to the same place' do
+        lambda do
+          config.parse(%w{--format standard --out file1 --format progress --out file1})
+        end.should raise_error('All but one formatter must use --out, only one can print to each stream (or STDOUT)')
+      end
+
+      it 'should associate --out to previous --format' do
+        config.parse(%w{--format progress --out file1 --format profile --out file2})
+        config.formats.should == [['progress', 'file1'], ['profile', 'file2']]
+      end
+
+      it 'should accept same --format options with same --out streams and keep only one' do
+        config.parse(%w{--format html --out file --format standard --format html --out file})
+        config.formats.should == [['standard', out], ['html', 'file']]
+      end
+
+      it 'should accept same --format options with different --out streams' do
+        config.parse(%w{--format html --out file1 --format html --out file2})
+        config.formats.should == [['html', 'file1'], ['html', 'file2']]
+      end
+
+      it 'should accept multiple --name options' do
+        config.parse(['--name', 'User logs in', '--name', 'User signs up'])
+
+        config.options[:name_regexps].should include(/User logs in/)
+        config.options[:name_regexps].should include(/User signs up/)
+      end
+
+      it 'should accept multiple -n options' do
+        config.parse(['-n', 'User logs in', '-n', 'User signs up'])
+
+        config.options[:name_regexps].should include(/User logs in/)
+        config.options[:name_regexps].should include(/User signs up/)
+      end
+
+      it 'should allow specifying environment variables in profiles' do
+        with_this_configuration({'selenium' => 'DRIVER=selenium'})
+        config.parse(['--profile', 'selenium'])
+        ENV['DRIVER'].should == 'selenium'
+        config.spec_files.should_not include('DRIVER=selenium')
+      end
+
+      describe 'Dry run execution' do
+        it 'returns true when --dry-run was specified on in the arguments' do
+          config.parse(['--dry-run'])
+          config.dry_run?.should be_true
+        end
+
+        it 'returns true when --dry-run was specified in a profile' do
+          with_this_configuration({'default' => '--dry-run'})
+          config.parse([])
+          config.dry_run?.should be_true
+        end
+
+        it 'returns false by default' do
+          config.parse([])
+          config.dry_run?.should be_false
+        end
+      end
+      
+      describe 'Specifying matcher type' do
+        it 'returns the matcher type when it was set' do
+          config.parse(['--matcher-type', 'classic'])
+          config.matcher_type.should eql :classic
+        end
+
+        it 'returns the matcher type when it was set with shorthand option' do
+          config.parse(['-I', 'classic'])
+          config.matcher_type.should eql :classic
+        end
+
+        it 'returns the default matcher type if it was not set' do
+          config.parse([])
+          config.matcher_type.should eql :regexp
+        end
+      end
+
+      describe 'handling tags' do
+        it 'returns an empty expression when no tags are specified' do
+          config.parse([])
+          config.tag_expression.should be_empty
+        end
+
+        it 'returns an expression when tags are specified' do
+          config.parse(['--tags','@smoke'])
+          config.tag_expression.should_not be_empty
+        end
+      end
+
+      describe 'Getting a backtrace of errors' do
+        before do
+          Lucid.use_full_backtrace = false
+        end
+
+        it 'should show full backtrace when --backtrace is called for' do
+          config = App.new(['--backtrace'])
+          begin
+            'x'.should == 'y'
+          rescue => e
+            e.backtrace[0].should_not == "#{__FILE__}:#{__LINE__ - 2}"
+          end
+        end
+        
+        after do
+          Lucid.use_full_backtrace = false
+        end
       end
       
     end
